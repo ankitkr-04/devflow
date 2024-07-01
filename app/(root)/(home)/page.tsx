88ae19ddfe061f8d8148b3dd854da4e5be43d7b3
ak0182274 <ak0182274@gmail.com>
2024-07-01 06:02:44 +0000

    implement basic navbr and install shadcn

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,11 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <p className='h2-bold'>At Home Page</p>
-      <UserButton afterSignOutUrl='/' />
-
+      <p className="h2-bold">At Home Page</p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,9 @@ export default function RootLayout({
             },
           }}
         >
-          <ThemeProvider>{children}</ThemeProvider>
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
         </ClerkProvider>
       </body>
     </html>
